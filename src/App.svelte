19a8a4e0a32dd4e3def7ec6b35e11685045c9d9b
mtempleheald <mtempleheald@users.noreply.github.com>
2021-03-26 20:24:06 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+import QuestionSet from "./QuestionSet.svelte";
+</script>
+
+
+<header>
+	<h1>Question Set demo</h1>
+	<p>This question set data came from a json file, including markdown for pre and post question</p>
+</header>
+<QuestionSet/>
+
+
+
+<style>
+  /* alternative approach at https://github.com/josefaidt/svelte-themer */
+  :root {
+    --colour-background: lightblue;
+	--colour-highlight: khaki;
+    --colour-text: black;
+	--border-style: solid;
+  }
+</style>
