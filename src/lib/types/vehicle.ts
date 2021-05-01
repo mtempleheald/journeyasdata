f4702446bb4fc73532bd7629597db620e3e8f2e4
Mark Temple-Heald <mtempleheald@hotmail.com>
2021-05-01 10:31:10 +0100

    component-less vehicle lookup

---
+++ src/lib/types/vehicle.ts
@@ -0,0 +1,13 @@
+export type VehicleType = {
+    "registration": string;
+    "make": string;
+    "model": string;
+    "body": string;
+    "doors": string;
+    "engine": string;
+    "colour": string;
+    "transmission": string;
+    "enginecc": string;
+    "enginebhp": string;
+    "abicode": string;
+}
