d0d01bb883f15d2dd9186840d66349099b1fb18a
Mark Temple-Heald <mtempleheald@hotmail.com>
2021-05-08 18:14:01 +0100

    fix error, erm, error

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,6 +1,14 @@
-<script>
-	export let status;
-	export let error;
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: { error, status }
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let status: number;
+	export let error: Error;
 </script>
 
 <svelte:head>
