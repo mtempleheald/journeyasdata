bed2ffbc3bc52deac34f0b1eb89c550775af16a9
mtempleheald <mtempleheald@users.noreply.github.com>
2021-08-03 20:53:49 +0100

    remove narolecash example, offers nothing extra on dangertent

--- src/lib/actions/actionprovider.ts
+++ src/lib/actions/actionprovider.ts
@@ -1,12 +1,10 @@
 import { actions as technicaldemo} from '$lib/actions/technicaldemo';
 import { actions as dangertentinsurance} from '$lib/actions/dangertentinsurance';
-import { actions as narolecash} from '$lib/actions/narolecash';
 
 export const getActions = function(brand: string) {
     switch (brand) {
         case "technicaldemo": return technicaldemo;
         case "dangertentinsurance": return dangertentinsurance;
-        case "narolecash" : return narolecash;
         default: return {}
     }
 }
