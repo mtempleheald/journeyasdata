00359c374db2b35c884425d02948d6d11b43f2d5
mtempleheald <mtempleheald@users.noreply.github.com>
2021-09-25 11:32:18 +0100

    upgrade kit and fix filepath issue

--- src/routes/api/journey/[brand].ts
+++ src/routes/api/journey/[brand].ts
@@ -1,11 +1,19 @@
 import type { JourneyType } from '$lib/types/journey'
 import path from 'path'
+import fs from 'fs'
 
 /* @type {import('@sveltejs/kit').RequestHandler} */
 export async function get({ params }) {
 
     console.log('loading journey')
-    const result: JourneyType = await import(path.resolve('static',params.brand,'journey.json')).then(module => module.default)
+    // TODO: rework the journey loading to avoid reloads mid journey
+    // static files found in /build/assets after build
+    var j = path.resolve('assets',params.brand,'journey.json')
+    if (!fs.existsSync(j)) {
+        // static files found in /static during development
+        j = path.resolve('static',params.brand,'journey.json')
+    }
+    const result: JourneyType = await import(j).then(module => module.default)
 
     return {
         body: result
