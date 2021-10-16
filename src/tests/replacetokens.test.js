3e7d9189dfa4c8c6fa94d3176cb4a84a8557c01d
Mark Temple-Heald <mtempleheald@hotmail.com>
2021-10-16 13:33:24 +0100

    add token replace tests

---
+++ src/tests/replacetokens.test.js
@@ -0,0 +1,57 @@
+import { replaceTokens } from '$lib/utils/replacetokens'
+
+test('string identifier', () => {
+    let identifier = "string containing {{componentid}}"
+    let displayValueStore = {"componentid": "displayvalue"}
+    let expected = "string containing displayvalue"
+    expect(replaceTokens(identifier, displayValueStore)).toEqual(expected)
+})
+
+test('string identifier with whitespace', () => {
+    let identifier = "string containing {{ componentid    }}"
+    let displayValueStore = {"componentid": "displayvalue"}
+    let expected = "string containing displayvalue"
+    expect(replaceTokens(identifier, displayValueStore)).toEqual(expected)
+})
+
+test('numeric identifier, numeric in store', () => {
+    let identifier = "string containing {{1}}"
+    let displayValueStore = {1: "displayvalue"}
+    let expected = "string containing displayvalue"
+    expect(replaceTokens(identifier, displayValueStore)).toEqual(expected)
+})
+
+test('numeric identifier, string in store', () => {
+    let identifier = "string containing {{1}}"
+    let displayValueStore = {"1": "displayvalue"}
+    let expected = "string containing displayvalue"
+    expect(replaceTokens(identifier, displayValueStore)).toEqual(expected)
+})
+
+test('string identifier, with repeat index', () => {
+    let identifier = "string containing {{componentid.1}}"
+    let displayValueStore = {"componentid.1": "displayvalue"}
+    let expected = "string containing displayvalue"
+    expect(replaceTokens(identifier, displayValueStore)).toEqual(expected)
+})
+
+test('numeric identifier, string in store, with repeat index', () => {
+    let identifier = "string containing {{1.1}}"
+    let displayValueStore = {"1.1": "displayvalue"}
+    let expected = "string containing displayvalue"
+    expect(replaceTokens(identifier, displayValueStore)).toEqual(expected)
+})
+
+test('numeric identifier, nummeric in store, with repeat index', () => {
+    let identifier = "string containing {{1.1}}"
+    let displayValueStore = {1.1: "displayvalue"}
+    let expected = "string containing displayvalue"
+    expect(replaceTokens(identifier, displayValueStore)).toEqual(expected)
+})
+
+test('identifier not in store should replace with empty string', () => {
+    let identifier = "string containing {{componentid}}"
+    let displayValueStore = {}
+    let expected = "string containing "
+    expect(replaceTokens(identifier, displayValueStore)).toEqual(expected)
+})
