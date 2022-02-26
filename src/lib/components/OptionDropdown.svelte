7533ca1c7ce5aa0991b2556c6c690e5e651fd601
Rod Laycock <rod.laycock@gmail.com>
2022-02-26 16:15:54 +0000

    amended OptionDropdown so it uses onChange instead of onBlur

--- src/lib/components/OptionDropdown.svelte
+++ src/lib/components/OptionDropdown.svelte
@@ -67,7 +67,7 @@
 				value={component.value}
 				data-reference={component.refdata}
 				required={component.required}
-				on:blur={act}
+				on:change={act}
 			>
 				<option value="">{!component.placeholder ? '-- select --' : component.placeholder}</option>
 
