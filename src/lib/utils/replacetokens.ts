fed9707123cf0039ea63064ebb255ea853fe5b47
mtempleheald <mtempleheald@users.noreply.github.com>
2022-03-08 22:29:53 +0000

    fixes #112 - rework replace tokens to use state store

--- src/lib/utils/replacetokens.ts
+++ src/lib/utils/replacetokens.ts
@@ -1,8 +1,31 @@
+/**
+ * Replace dynamic content tokens with display values from the state store 
+ */
+export const replace_tokens = function (input: string, state: StateStoreType) {
+	if (!input) return '';
+
+	let output: string = input;
+
+	// a valid key can only consist of alphanumeric characters or . (full stop) which is only allowed within repeating groups
+	const re = new RegExp(/\{\{\s*(\w|\.)*\s*\}\}/gi);
+	const tokens = input.match(re); // ['{{ tokentobereplaced }}', '{{anothertokentobereplaced}}']
+
+	tokens &&
+		tokens.forEach((t) => {
+			// extract token id minus the brackets and any whitespace
+			const id = t.substring(2, t.length - 2).trim();
+			// Replace tokens with their values from the store, empty string if not found
+			output = output.replace(t, `<output for="${t}">${state[id.toString()]?.display ?? ''}</output>`);
+		});
+
+	return output;
+}
+
+
 /*
  ** Given an input string containing one or more {{token}} and a key-value list of replacement values
  ** Return a string with all tokens replaced
  */
-
 export const replaceTokens = function (input: string, values: object) {
 	if (!input) return '';
 
