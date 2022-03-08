89cc12286c90bf05dd1e8277da336b149a7a6a23
mtempleheald <mtempleheald@users.noreply.github.com>
2022-03-08 19:49:03 +0000

    add validators based on state store

--- src/lib/utils/validators.ts
+++ src/lib/utils/validators.ts
@@ -18,34 +18,106 @@ export function component_valid(component: InputComponent, state: StateStoreType
 	if (!component.id) {
 		return true;
 	}
-
 	// (passed|failed) - input component has been attempted and has a validation status, trust this value
 	if (state[component.id]?.valid != null) {
 		return state[component.id]?.valid;
 	}
-
 	// (skipped) - component is optional and has been skipped, don't need to validate
 	if (!component.required ?? false) {
 		return true;
 	}
-
 	// (hidden) - component is hidden due to dependency on another component, don't fail validation
 	if (
 		component.dependsupon &&
 		component.dependsupon.value != state[component.dependsupon.id]?.value
-	){
+	) {
 		return true;
 	}
-
 	// (missing) - component has not been answered, yet is required and not hidden, so it must be invalid
 	return false;
 }
 
+/**
+ * Establish section validity by checking each component within it, if any component is invalid, so is the section
+ * @param section           Section metadata from journey.json used to establish validation criteria
+ * @param valueStore        $valueStore from the SvelteKit runtime, or an object consisting of simple string key value pairs
+ * @param validationStore   $validationStore from the SvelteKit runtime, or an object consisting of simple string key value pairs
+ * @returns                 boolean
+ */
+export function section_valid(
+	section: SectionType | RepeatingGroupType,
+	state: StateStoreType
+): boolean {
+	// function duplicated from Repeatinggroup.svelte
+	function update_section(section: SectionType, index: number) {
+		const newComponents: ComponentType[] = section.components.map((comp) => {
+			return {
+				...comp,
+				id: comp.id ? `${comp.id}.${index}` : undefined
+			};
+		});
+		return { ...section, iteration: index, components: newComponents };
+	}
+	switch (section.type) {
+		case 'repeatinggroup':
+			return section.sections.every((s, i) => section_valid(update_section(s, i), state));
+		default:
+			return section.components.every((c) => component_valid(c as InputComponent, state));
+	}
+}
+
+/**
+ * Establish page validity by checking each section within it, if any section is invalid, so is the page
+ * @param page              Page metadata from journey.json used to establish validation criteria
+ * @param valueStore        $valueStore from the SvelteKit runtime, or an object consisting of simple string key value pairs
+ * @param validationStore   $validationStore from the SvelteKit runtime, or an object consisting of simple string key value pairs
+ * @returns                 boolean
+ */
+export function page_valid(page: PageType, state: StateStoreType): boolean {
+	return page.sections.every((s) => section_valid(s, state));
+}
+
+/**
+ * Establish journey validity by checking each page within it, if any page is invalid, so is the journey
+ * @param journey           Journey metadata from journey.json used to establish validation criteria
+ * @param valueStore        $valueStore from the SvelteKit runtime, or an object consisting of simple string key value pairs
+ * @param validationStore   $validationStore from the SvelteKit runtime, or an object consisting of simple string key value pairs
+ * @returns                 boolean
+ */
+export function journey_valid(journey: JourneyType, state: StateStoreType): boolean {
+	return journey.pages.every((p) => page_valid(p, state));
+}
+
+export function get_first_invalid_component_in_section(
+	section: SectionType,
+	state: StateStoreType
+): string | null {
+	return section.components.find((c) => !component_valid(c, state));
+}
+
+export function get_first_invalid_component_in_page(
+	page: PageType,
+	state: StateStoreType
+): string | null {
+	return page.sections
+		.flatMap((s) => {
+			if (s.type == 'repeatinggroup') {
+				return to_section_list(s).flatMap((ss) => {
+					return ss.components.map((c) => {
+						return c;
+					});
+				});
+			} else {
+				return s.components.map((c) => {
+					return c;
+				});
+			}
+		})
+		.find((x) => !component_valid(x, state));
+}
+
 /*
-TODO: 
-  - valueStore | validationStore | displayValueStore to be removed in favour of stateStore.
-  - functions to use snake_case as it is easier to read
-  - rework functions below here to meet the above, update/create tests, delete the rest
+TODO: Remove everything below here
 */
 
 /**
--- src/test/utils/validators.test.ts
+++ src/test/utils/validators.test.ts
@@ -1,28 +1,117 @@
 import type { InputComponent } from '$lib/types/journey';
 import { componentValid, component_valid } from '$lib/utils/validators';
 
-test('InputComponent valid if state store says it is', () => {
+test('InputComponent valid if state valid', () => {
 	const comp: InputComponent = {
 		id: '1',
 		type: 'Text'
 	};
 	const state: StateStoreType = {
 		'1': {
-			value: '',
-			display: '',
+			value: undefined,
+			display: undefined,
 			valid: true
-		}		
-	}
-	expect (component_valid(comp, state)).toBe(true);
-})
-// TODO: rework all below functions to use state store
+		}
+	};
+	expect(component_valid(comp, state)).toBe(true);
+});
+test('InputComponent invalid if state invalid', () => {
+	const comp: InputComponent = {
+		id: '1',
+		type: 'Text'
+	};
+	const state: StateStoreType = {
+		'1': {
+			value: undefined,
+			display: undefined,
+			valid: false
+		}
+	};
+	expect(component_valid(comp, state)).toBe(false);
+});
+test('InputComponent valid if not required and skipped', () => {
+	const comp: InputComponent = {
+		id: '1',
+		type: 'Text'
+	};
+	const state: StateStoreType = {};
+	expect(component_valid(comp, state)).toBe(true);
+});
+test('InputComponent invalid if required and skipped', () => {
+	const comp: InputComponent = {
+		id: '1',
+		type: 'Text',
+		required: true
+	};
+	const state: StateStoreType = {};
+	expect(component_valid(comp, state)).toBe(false);
+});
 
+test('InputComponent valid if required but skipped due to dependency', () => {
+	const comp: InputComponent = {
+		id: '2',
+		type: 'Text',
+		required: true,
+		dependsupon: {
+			id: '1',
+			value: 'Y'
+		}
+	};
+	const state: StateStoreType = {
+		'1': {
+			value: 'N',
+			display: 'No',
+			valid: true
+		}
+	};
+	expect(component_valid(comp, state)).toBe(true);
+});
+test('InputComponent invalid if required and not skipped due to dependency', () => {
+	const comp: InputComponent = {
+		id: '2',
+		type: 'Text',
+		required: true,
+		dependsupon: {
+			id: '1',
+			value: 'Y'
+		}
+	};
+	const state: StateStoreType = {
+		'1': {
+			value: 'Y',
+			display: 'Yes',
+			valid: true
+		}
+	};
+	expect(component_valid(comp, state)).toBe(false);
+});
+test('InputComponent valid within a repeating group', () => {
+	const comp: InputComponent = {
+		id: 'componentid.0',
+		type: 'Text'
+	};
+	const state: StateStoreType = {
+		'1': {
+			value: undefined,
+			display: undefined,
+			valid: true
+		}
+	};
+	expect(component_valid(comp, state)).toBe(true);
+});
 
+// Display components (no id required)
+test('DisplayBlock valid regardless of state', () => {
+	const comp = {
+		type: 'Displayblock',
+		content: 'test'
+	};
+	const state: StateStoreType = {};
+	// @ts-ignore
+	expect(component_valid(comp, state)).toBe(true);
+});
 
-
-
-
-
+// TODO: remove everything below here
 
 test('input provided, valid', () => {
 	const comp: InputComponent = {
